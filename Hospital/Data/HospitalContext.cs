7f4bfa05471306920773c710a36d9604edb9c2a6
Dilyana <dilyana_stz@abv.bg>
2018-07-14 19:44:15 +0300

    Migrations

---
+++ Hospital/Data/HospitalContext.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data;
+using P01_HospitalDatabase.Data.Models;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext : DbContext
+    {
+
+        public HospitalContext() { }
+
+        public HospitalContext(DbContextOptions options) 
+            :base(options) { }
+
+
+        public DbSet<Patient> Patients { get; set; }
+
+        public DbSet<Medicament> Medicaments { get; set; }
+
+        public DbSet<Visitation> Visitations { get; set; }
+
+        public DbSet<Diagnose> Diagnoses { get; set; }
+
+        public DbSet<PatientMedicament> PatientsMedicaments { get; set; }
+
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>(entity =>
+            {
+                entity.HasKey(e => e.PatientId);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .IsUnicode(true)
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .IsUnicode(true)
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .IsUnicode()
+                    .HasMaxLength(250);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .IsUnicode(false)
+                    .HasMaxLength(80);
+
+                entity.Property(e => e.HasInsurance)
+                    .HasDefaultValue(true);
+
+            });
+
+
+            modelBuilder.Entity<Visitation>(entity =>
+            {
+                entity.HasKey(e => e.VisitationId);
+
+                entity.Property(e => e.Date)
+                    .IsRequired()
+                    .HasColumnType("DATETIME2")
+                    .HasDefaultValueSql("GETDATE()");
+
+                entity.Property(e => e.Comments)
+                    .IsRequired()
+                    .IsUnicode(true)
+                    .HasMaxLength(250);
+
+                entity.Property(e => e.PatientId)
+                    .IsRequired()
+                    .IsUnicode()
+                    .HasMaxLength(250);
+
+                entity.HasOne(v => v.Patient)
+                    .WithMany(p => p.Visitations)
+                    .HasForeignKey(e => e.PatientId)
+                    .HasConstraintName("FK_Visitation_Patient");
+
+                entity.Property(p => p.DoctorId)
+                    .IsRequired(false);
+
+                entity.HasOne(p => p.Doctor)
+                    .WithMany(d => d.Visitations)
+                    .HasForeignKey(p => p.DoctorId);
+
+            });
+
+            modelBuilder.Entity<Diagnose>(entity =>
+            {
+                entity.HasKey(e => e.DiagnoseId);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .IsUnicode(true)
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Comments)
+                    .IsRequired()
+                    .IsUnicode(true)
+                    .HasMaxLength(250);
+
+                entity.Property(e => e.PatientId)
+                    .IsRequired()
+                    .IsUnicode()
+                    .HasMaxLength(250);
+
+                entity.HasOne(v => v.Patient)
+                    .WithMany(p => p.Diagnoses)
+                    .HasForeignKey(e => e.PatientId)
+                    .HasConstraintName("FK_Diagnose_Patient");
+
+            });
+
+
+
+            modelBuilder.Entity<Medicament>(entity =>
+            {
+                entity.HasKey(e => e.MedicamentId);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .IsUnicode(true)
+                    .HasMaxLength(50);
+
+            });
+
+            modelBuilder.Entity<PatientMedicament>(entity =>
+            {
+                entity.HasKey(e => new
+                {
+                    e.PatientId,
+                    e.MedicamentId
+
+
+                });
+
+                entity.HasOne(e => e.Medicament)
+                    .WithMany(e => e.Prescriptions)
+                    .HasForeignKey(e => e.MedicamentId);
+
+                entity.HasOne(e => e.Patient)
+                    .WithMany(e => e.Prescriptions)
+                    .HasForeignKey(e => e.PatientId);
+            });
+
+            modelBuilder.Entity<Doctor>(
+                
+                entity => { entity.HasKey(p => p.DoctorId);
+                    
+                entity.Property(p => p.Name)
+                    .IsUnicode(true)
+                    .HasMaxLength(100)
+                    .IsRequired();
+
+                entity.Property(p => p.Specialty)
+                    .IsUnicode(true)
+                    .HasMaxLength(100)
+                    .IsRequired();
+
+                
+            });
+        }
+
+}
+
+}
